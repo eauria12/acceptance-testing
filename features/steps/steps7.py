702b5a499504e9164e90ef7272726c8bef101959
eauria12 <85134383+eauria12@users.noreply.github.com>
2023-08-01 23:26:32 -0500

    Create T2.json

--- features/steps/steps7.py
+++ features/steps/steps7.py
@@ -5,9 +5,13 @@ from task import Task
 @given('the to-do list contains all tasks with various due dates')
 def step_impl(context):
     # Set the to-do list as an empty list
-    context.task1 = Task(1,"pepe","2023-12-12",False)
+    context.task1 = Task(1,"medio","2023-12-12",False)
+    context.task2 = Task(1,"inicial","2022-12-12",False)
+    context.task3 = Task(1,"final","2024-12-12",False)
     context.manager = ToDoListManager()
     context.manager.add_task(context.task1)
+    context.manager.add_task(context.task2)
+    context.manager.add_task(context.task3)
 
 # Step 2: When the user adds a task "Buy groceries"
 @when('the user chooses to sort tasks by due date')
@@ -18,4 +22,4 @@ def step_impl(context):
 @then('the to-do list should show tasks sorted by due date in ascending order')
 def step_impl(context):
     # Check if the task is in the to-do list
-    assert context.task1 in context.manager.tasks, f'Task not found in the to-do list'
+    assert context.task2 == context.manager.tasks[0], f'Task not found in the to-do list'
